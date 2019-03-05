ae6b110951d58134084716dcf8c207775a20d714
rzepkamichal <michrzep@interia.pl>
2019-03-05 19:15:39 +0100

    created basic root and training overview layout

--- app/src/model/Training.java
+++ app/src/model/Training.java
@@ -1,4 +1,71 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Training {
+
+    private final StringProperty trainingName;
+    private IntegerProperty amountOfEntrances;
+    private ObjectProperty<LocalDate> lastActivityDate;
+
+
+    public Training(){
+        this(null,null,0);
+    }
+
+    @JsonCreator
+    public Training(@JsonProperty("name") String trainingName,
+                    @JsonProperty("date") LocalDate lastActivityDate,
+                    @JsonProperty("entrances") int amountOfEntrances) {
+
+        this.trainingName = new SimpleStringProperty(trainingName);
+        this.amountOfEntrances = new SimpleIntegerProperty(amountOfEntrances);
+        this.lastActivityDate = new SimpleObjectProperty<LocalDate>(LocalDate.from(lastActivityDate));
+    }
+
+    public String getTrainingName() {
+        return trainingName.get();
+    }
+
+    public StringProperty trainingNameProperty() {
+        return trainingName;
+    }
+
+    public void setTrainingName(String trainingName) {
+        this.trainingName.set(trainingName);
+    }
+
+    public int getAmountOfEntrances() {
+        return amountOfEntrances.get();
+    }
+
+    public IntegerProperty amountOfEntrancesProperty() {
+        return amountOfEntrances;
+    }
+
+    public void setAmountOfEntrances(int amountOfEntrances) {
+        this.amountOfEntrances.set(amountOfEntrances);
+    }
+
+    public LocalDate getLastActivityDate() {
+        return lastActivityDate.get();
+    }
+
+    public ObjectProperty<LocalDate> lastActivityDateProperty() {
+        return lastActivityDate;
+    }
+
+    public void setLastActivityDate(LocalDate lastActivityDate) {
+        this.lastActivityDate.set(lastActivityDate);
+    }
+
+
 }
